3cf70e81d1f2f57f3020ee138e376b62212b6cd2
Hans Meine <hans_meine@gmx.net>
2012-01-19 08:14:04 +0100

    get rid of PIL dependency just for PPM loading (incl. own parser) also removes seeking requirement (no StringIO anymore)

--- pdftoppm_renderer.py
+++ pdftoppm_renderer.py
@@ -1,17 +1,44 @@
-import subprocess, cStringIO, PIL.Image, numpy
+import subprocess, numpy, string
 
 def renderPDFPage(pdfFilename, pageIndex, (widthPX, heightPX)):
-    pdftoppm = subprocess.Popen(('pdftoppm -f %(pageIndex)d -l %(pageIndex)d '
-                                 '-scale-to-x %(widthPX)d '
-                                 '-scale-to-y %(heightPX)d' % locals()).split() +
-                                [pdfFilename],
-                                 stdout = subprocess.PIPE)
-    ppmData, _ = pdftoppm.communicate()
-    assert pdftoppm.returncode == 0
-    if not ppmData:
-        return None # in the future (when we have PDF metadata), could raise error instead
+    command = ('pdftoppm -f %(pageIndex)d -l %(pageIndex)d '
+               '-scale-to-x %(widthPX)d '
+               '-scale-to-y %(heightPX)d' % locals()).split()
+    command.append(pdfFilename)
 
-    ppm = cStringIO.StringIO(ppmData)
-    img = PIL.Image.open(ppm)
-    result = numpy.asanyarray(img)
+    pdftoppm = subprocess.Popen(command, stdout = subprocess.PIPE)
+
+    result = readPPM(pdftoppm.stdout)
+
+    pdftoppm.wait()
+    assert pdftoppm.returncode == 0
+
     return result
+
+def readPPM(file):
+    def _readWord(prefix = ""):
+        result = prefix
+        while True:
+            ch = file.read(1)
+            if not ch:
+                return None
+            elif ch in string.whitespace:
+                if result:
+                    break
+                continue
+            elif ch == '#':
+                file.readline()
+            result += ch
+        return result
+
+    header = _readWord()
+    assert header == "P6"
+    
+    width = int(_readWord())
+    height = int(_readWord())
+    maxVal = int(_readWord())
+    assert maxVal < 65536
+
+    return numpy.fromfile(file,
+                          dtype = numpy.uint8 if maxVal < 256 else numpy.uint16,
+                          count = width * height * 3).reshape((height, -1, 3))
