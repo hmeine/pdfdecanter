b79951510a41852c4e54faee7115750b99fac985
Hans Meine <hans_meine@gmx.net>
2012-04-21 07:06:27 +0200

    add cache format versioning

--- cache.py
+++ cache.py
@@ -8,23 +8,22 @@ def _writePatches(group, patches):
         ds.attrs['pos'] = (pos.x(), pos.y())
 
 def writeSlides(filename, slides):
-    f = h5py.File(filename)
+    with h5py.File(filename) as f:
+        f.attrs['format_version'] = slide.Presentation.FORMAT_VERSION
 
-    for i, s in enumerate(slides):
-        sg = f.create_group("slide%d" % i)
-        sg.attrs['size'] = (s.size().width(), s.size().height())
+        for i, s in enumerate(slides):
+            sg = f.create_group("slide%d" % i)
+            sg.attrs['size'] = (s.size().width(), s.size().height())
 
-        g = sg.create_group('header')
-        _writePatches(g, s.header())
+            g = sg.create_group('header')
+            _writePatches(g, s.header())
 
-        g = sg.create_group('footer')
-        _writePatches(g, s.footer())
+            g = sg.create_group('footer')
+            _writePatches(g, s.footer())
 
-        for j, frame in enumerate(s._frames):
-            g = sg.create_group("frame%d" % j)
-            _writePatches(g, frame)
-
-    f.close()
+            for j, frame in enumerate(s._frames):
+                g = sg.create_group("frame%d" % j)
+                _writePatches(g, frame)
 
 def _readPatches(group):
     result = []
@@ -39,23 +38,23 @@ def _readPatches(group):
 def readSlides(filename):
     slides = []
 
-    f = h5py.File(filename)
+    with h5py.File(filename) as f:
+        if f.attrs.get('format_version', 1) < slide.Presentation.FORMAT_VERSION:
+            return None
 
-    for i in range(len(f)):
-        sg = f['slide%d' % i]
-        w, h = sg.attrs['size']
-        s = slide.Slide(QtCore.QSizeF(w, h))
-        s._header = _readPatches(sg['header'])
-        s._footer = _readPatches(sg['footer'])
-        for j in range(len(sg)):
-            try:
-                fg = sg['frame%d' % j]
-            except KeyError:
-                pass
-            else:
-                s._frames.append(_readPatches(fg))
-        slides.append(s)
-
-    f.close()
+        for i in range(len(f)):
+            sg = f['slide%d' % i]
+            w, h = sg.attrs['size']
+            s = slide.Slide(QtCore.QSizeF(w, h))
+            s._header = _readPatches(sg['header'])
+            s._footer = _readPatches(sg['footer'])
+            for j in range(len(sg)):
+                try:
+                    fg = sg['frame%d' % j]
+                except KeyError:
+                    pass
+                else:
+                    s._frames.append(_readPatches(fg))
+            slides.append(s)
 
     return slides
