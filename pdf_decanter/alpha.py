d929fcf45e374c451861f06c2ef77c79f81a8a41
Hans Meine <hans_meine@gmx.net>
2013-05-24 20:11:23 +0200

    extract method unblend_alpha_1d

--- pdf_decanter/alpha.py
+++ pdf_decanter/alpha.py
@@ -1,17 +1,26 @@
 from PyQt4.QtGui import QImage, QPainter
 import qimage2ndarray, numpy
 
+
+def unblend_alpha_1d(rgb, bg, c):
+    rgb = numpy.require(rgb, dtype = numpy.float64)
+    bg  = numpy.require(bg,  dtype = numpy.float64)
+    alpha = (numpy.sum(numpy.abs(rgb + 0.5 - bg), 0) /
+             numpy.sum(numpy.abs(c[:,None] - bg), 0))
+    alpha *= 255
+    alpha = numpy.floor(alpha).clip(0, 255)
+    return alpha
+
+
 def unblend_alpha(rgb, bg, c):
     rgb, bg = numpy.broadcast_arrays(rgb, bg)
     diff = rgb - bg
     changed_y, changed_x = numpy.nonzero(diff.any(-1))
 
-    rgb_i = rgb[changed_y, changed_x].T.astype(numpy.float64)
-    bg_i = bg[changed_y, changed_x].T.astype(numpy.float64)
+    rgb_i = rgb[changed_y, changed_x].T
+    bg_i  = bg [changed_y, changed_x].T
 
-    alpha_i = (numpy.sum(numpy.abs(rgb_i + 0.5 - bg_i), 0)) / numpy.sum(numpy.abs(c[:,None] - bg_i), 0)
-    alpha_i *= 255
-    alpha_i = numpy.floor(alpha_i).clip(0, 255)
+    alpha_i = unblend_alpha_1d(rgb_i, bg_i, c)
 
     alpha = numpy.zeros(bg.shape[:2])
     alpha[changed_y, changed_x] = alpha_i
