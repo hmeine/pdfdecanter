99ffde72d12bcf03ee87ed422275c6ead34a77e1
Hans Meine <hans_meine@gmx.net>
2014-04-21 07:55:27 +0200

    add copyright headers to most source files

--- pdf_decanter/dynqt.py
+++ pdf_decanter/dynqt.py
@@ -1,3 +1,17 @@
+#  Copyright 2014-2014 Hans Meine <hans_meine@gmx.net>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import sys, os
 
 def getprop_PythonQt(prop):
@@ -6,7 +20,7 @@ def getprop_PythonQt(prop):
 	Used on getters that have the same name as a corresponding
 	property.  For PythonQt, this version will just return the
 	argument, which is assumed to be (the value of) a python property
-	with which PythonQt exposes Qt properties."""
+	through which PythonQt exposes Qt properties."""
 	return prop
 
 def getprop_other(getter):
